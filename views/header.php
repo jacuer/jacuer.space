404b1caff008c084f0326a5475bd01da889ca79b
Juan Cuero <juancuero@gmail.com>
2022-08-04 22:51:30 +0200

    barra de navegacion header

--- views/header.php
+++ views/header.php
@@ -27,7 +27,7 @@ if( ($_SESSION['ok']) && isset($_SESSION['role']))
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarScroll">
-          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll ms-auto" style="--bs-scroll-height: 100px;">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="admin">Home</a>
             </li>           
